11858973f99c0469c5ac147a4eb1926d9fdc4b57
Artem Borodovoi <zzzfisherzzz@gmail.com>
2021-08-06 19:56:59 +0300

    work with styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,15 @@
 import $ from "./lib/lib";
 
-$("button").on("click", function () {
-  $("div").eq(1).toggleClass("active");
+$("#first").on("click", () => {
+  $("div").eq(1).fadeToggle(800);
 });
 
-$("div").click(function () {
-  console.log($(this).index());
+$('[data-count="second"]').on("click", () => {
+  $("div").eq(2).fadeToggle(800);
 });
 
-// console.log($('div').eq(2).find('.some'));
-// console.log($('.some').closest('.findme'));
-$("button").fadeIn(1800);
-
-// console.log($('button').html('Hello'))
+$("button")
+  .eq(2)
+  .on("click", () => {
+    $(".w-500").fadeToggle(800);
+  });
